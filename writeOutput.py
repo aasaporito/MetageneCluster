56501d07927b5ee6e92468836790cb1274ae4dea
Aaron <sapblatt11@gmail.com>
2023-06-05 16:08:42 -0400

    Removed unused imports, improved formatting

--- writeOutput.py
+++ writeOutput.py
@@ -1,47 +1,42 @@
 """Summary
 """
-from plot import genPlot
 import os
 
 
-
-
-#create txt list of features for a cluster
+# create txt list of features for a cluster
 def writeNames(names, fileName, dirName):
     """Summary
-    
+
     Args:
         names (TYPE): Description
         fileName (TYPE): Description
         dirName (TYPE): Description
     """
-    with open('Outputs/'+dirName+'/'+fileName , 'w') as record: 
-        #write header
+    with open('Outputs/' + dirName + '/' + fileName, 'w') as record:
+        # write header
 
-        #write names
+        # write names
         for name in names:
             line = str(name)
             record.write(line)
-    record.close()  
+    record.close()
+
 
 def makeDir(name):
     """Summary
-    
+
     Args:
         name (TYPE): Description
-    
+
     Returns:
         TYPE: Description
     """
     newName = name
-    path = 'Outputs/'+newName
-    while os.path.exists(path):#path exists, create new sub file name
-       num = int(newName[-1]) + 1 
-       newName = name[:-1]+str(num)
-       path = 'Outputs/'+newName
-    
-    os.makedirs(path) 
-    return newName
-             
+    path = 'Outputs/' + newName
+    while os.path.exists(path):  # path exists, create new sub file name
+        num = int(newName[-1]) + 1
+        newName = name[:-1] + str(num)
+        path = 'Outputs/' + newName
 
-    
+    os.makedirs(path)
+    return newName
