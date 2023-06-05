93d7f74cbeabc549be66ca44a2ae8e3be3647b0a
Aaron <sapblatt11@gmail.com>
2023-06-05 16:24:24 -0400

    Removed unused references, reformatted file

--- plot.py
+++ plot.py
@@ -2,13 +2,12 @@
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import os 
 
 
-##add up/down stream markers and num features label 
-def genPlot(result,fname,dirName,udStream,numFeatures):    
+# add up/down stream markers and num features label
+def genPlot(result, fname, dirName, udStream, numFeatures):
     """Summary
-    
+
     Args:
         result (TYPE): Description
         fname (TYPE): Description
@@ -20,33 +19,32 @@ def genPlot(result,fname,dirName,udStream,numFeatures):
     #     x[i] = i
     #     #y[i] = result[i]
     # y= result
-    y=np.array(result)
-    x=np.arange(len(y))
-
+    y = np.array(result)
+    x = np.arange(len(y))
 
     # plot
     fig, ax = plt.subplots()
 
     ax.set(xlim=(0, len(result)), xticks=np.arange(0, len(result)),
-        ylim=(0, max(result) ), yticks=np.arange(0, max(result)+10))#result[1]
-
-    xyScale= 25
+           ylim=(0, max(result)), yticks=np.arange(0, max(result) + 10))  # result[1]
 
     ax.set_xlabel('Distance')
     ax.set_ylabel('Reads')
 
     plt.figure(figsize=(15, 10))
-    plt.plot(x[(len(x)-udStream-1):len(x)-1],y[(len(x)-udStream-1):len(x)-1],linestyle='-.',color='red')
-    plt.plot(x[0:udStream-1],y[0:udStream-1],linestyle='-.',color='red')
-    plt.plot(x[udStream-1:(len(x)-udStream-1)],y[udStream-1:(len(x)-udStream-1)],color ='black')
-    plt.title(fname+' ('+str(numFeatures)+')')
+    plt.plot(x[(len(x) - udStream - 1):len(x) - 1],
+             y[(len(x) - udStream - 1):len(x) - 1], linestyle='-.', color='red')
+    plt.plot(x[0:udStream - 1], y[0:udStream - 1], linestyle='-.', color='red')
+    plt.plot(x[udStream - 1:(len(x) - udStream - 1)],
+             y[udStream - 1:(len(x) - udStream - 1)], color='black')
+    plt.title(fname + ' (' + str(numFeatures) + ')')
     plt.xlabel('Distance')
     plt.ylabel('Reads')
     # plt.locator_params(axis='y', nbins=xyScale)
-    # plt.locator_params(axis='x',nbins=xyScale) 
-    
-    path=fname+".png" #in case of single plot
-    if dirName != None:
-        path = 'Outputs/'+dirName+'/'+path
+    # plt.locator_params(axis='x',nbins=xyScale)
 
-    plt.savefig(path , dpi = 75)
+    path = fname + ".png"  # in case of single plot
+    if dirName != None:
+        path = 'Outputs/' + dirName + '/' + path
+
+    plt.savefig(path, dpi=75)
