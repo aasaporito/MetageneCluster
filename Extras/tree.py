673130740f4f637e56f442998af37c804de1e608
Aaron <sapblatt11@gmail.com>
2023-06-06 10:56:03 -0400

    Completed documentation for tree.py

--- Extras/tree.py
+++ Extras/tree.py
@@ -1,77 +1,83 @@
 """Summary
+    Cluster node class for storage in a tree structure
 """
-#class tree:
-   
 
 
 class cluster:
 
     """Summary
-    
+        Cluster node class.
+
     Attributes:
-        dataIdxs (list): Description
-        distance (TYPE): Description
-        left (TYPE): Description
-        right (TYPE): Description
+        dataIdxs (list): List of data IDs (Used as node names)
+        distance (int): Distance
+        left (cluster): Left node
+        right (cluster): Right node
     """
-    
-    def __init__(self,distance=None,left=None,right=None) -> None:
+
+    def __init__(self, distance=None, left=None, right=None) -> None:
         """Summary
-        
+            Standard constructor for cluster
+    
         Args:
-            distance (None, optional): Description
-            left (None, optional): Description
-            right (None, optional): Description
+            distance (int, optional): Distance
+            left (cluster, optional): Left cluster in the tree from current
+            right (cluster, optional): Right cluster in the tree from current
         """
-        self.distance=distance
-        self.left = left 
-        self.right=right
+        self.distance = distance
+        self.left = left
+        self.right = right
         self.dataIdxs = []
 
-
-    def addIdx(self,idx):
+    def addIdx(self, idx):
         """Summary
-        
+            Adds an id to the dataIdxs list
+
         Args:
-            idx (TYPE): Description
+            idx (int): Integer to identify an entry
         """
         self.dataIdxs.append(idx)
-    
-
 
     def getIdxs(self):
         """Summary
-        
+            Gets idxs
         Returns:
-            TYPE: Description
+            list: 
         """
         return self.dataIdxs
+
     def isLeaf(self):
         """Summary
-        
+            Checks if the cluster is a leaf
+
         Returns:
-            TYPE: Description
+            bool: Returns true or None
         """
         if self.left == None:
-            return True 
+            return True
+
     def getRight(self):
         """Summary
-        
+            Gets the cluster to the right
+
         Returns:
-            TYPE: Description
+            cluster: Cluster to the right
         """
         return self.right
+
     def getLeft(self):
         """Summary
-        
+            Gets the cluster to the left
+
         Returns:
-            TYPE: Description
+            cluster: Cluster to the left
         """
         return self.left
+
     def getDistance(self):
         """Summary
-        
+            Gets the distance
         Returns:
-            TYPE: Description
+            int: Distance
         """
         return self.distance
