57618e4651b889facd0078a68686cd855c7004f7
Aaron <sapblatt11@gmail.com>
2023-06-06 09:12:18 -0400

    Staged for removal

--- Extras/cluster.py
+++ Extras/cluster.py
@@ -1,68 +1,73 @@
 """Summary
+
 """
 from dataclasses import dataclass
 
+#  todo 5 (general) +0: Is this file unused?
 @dataclass
-class cluster: 
+class cluster:
 
     """Summary
+
     """
-    
-    def __init__(self,data):
+
+    def __init__(self, data):
         """Summary
-        
+            Constructor for Cluster object
+
         Args:
-            data (TYPE): Description
+            data (list): Description
         """
-        #initialize random centers 
+        # initialize random centers
 
-        self._centers =[]
-        self._idxs=[]
-        self._data=[]
-        self._plus=0
-        self._minus=0
+        self._centers = []
+        self._idxs = []
+        self._data = []
+        self._plus = 0
+        self._minus = 0
 
-    #gettrs 
+    # gettrs
     def getCenters(self):
         """Summary
-        
+
         Returns:
             TYPE: Description
         """
         return self._centers
+
     def getIdxs(self):
         """Summary
-        
+
         Returns:
             TYPE: Description
         """
         return self._idxs
+
     def getData(self):
         """Summary
-        
+
         Returns:
             TYPE: Description
         """
         return self._data
+
     def strandRatio(self):
         """Summary
-        
+
         Returns:
             TYPE: Description
         """
-        return self._plus/(self._plus/self._minus) 
-    
-    def incStrand(self,strand):
+        return self._plus / (self._plus / self._minus)
+
+    def incStrand(self, strand):
         """Summary
-        
+
         Args:
             strand (TYPE): Description
         """
-        if strand =='+':
-            self._plus+=1
-        elif strand =='-':
-            self._minus+=1 
+        if strand == '+':
+            self._plus += 1
+        elif strand == '-':
+            self._minus += 1
 
-   # def _randCenters(self): 
-    
-    
+   # def _randCenters(self):
