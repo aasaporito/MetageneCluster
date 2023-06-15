9054785154eaa0550875593e7dd701dca2ede6a8
Aaron <sapblatt11@gmail.com>
2023-06-15 11:27:12 -0400

    Fixed output paths

--- writeOutput.py
+++ writeOutput.py
@@ -13,6 +13,8 @@ def writeNames(names, fileName, dirName):
         fileName (str): Name for the file
         dirName (str): Name for the output folder within Outputs/
     """
+
+
     with open('Outputs/' + dirName + '/' + fileName, 'w') as record:
         for name in names:
             line = str(name)
@@ -31,6 +33,8 @@ def makeDir(name):
     """
     newName = name
     path = 'Outputs/' + newName
+    print(name)
+    print(path)
     while os.path.exists(path):  # path exists, create new sub file name
         num = int(newName[-1]) + 1
         newName = name[:-1] + str(num)
