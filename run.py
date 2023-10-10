61aef37adaf211c576c1bc744db7b02a1383d355
sdouglass1 <73650672+sdouglass1@users.noreply.github.com>
2023-10-10 19:22:54 -0400

    Update run.py
    Changed how parameters are parsed to support additional parameters already added to documentation.  Also supported passing of a second sam_file to metaGenePlot() and dist_reduct to plot().

--- run.py
+++ run.py
@@ -4,10 +4,14 @@
 Attributes:
     clustering (int): Determines if we are making a single metagene plot (1), a set of k-means clustered plots (2), or both (3).
     shape_or_magnitude (str): Determines if we are clustering based on shape (1) or magnitude (2).
+    ratio_of_files (str): Determines if we are clustering based on a single input file (0) or the ratio of first file:second file (1).
     sam_file (str): The input file in .sam format that contains the genomic signal to be analyzed.
+    sam_file2 (str): The input file in .sam format that contains the genomic signal to divide sam_file's signal by when -r is enabled.
     gff_file (str): The input file in .gff format that contains the genomic regions to be analyzed.
     feature_type (str): The feature to be extracted from the gff for analysis, such as 'gene,' 'CDS,' or 'exon.'
     ud_stream (int): The amount of upstream and downstream sequence to include in the metagene plot(s).  Provided for reference, does not contribute to clustering calculations.
+    norm_length (int): The length to normalize features to.
+    dist_reduct (float): The number of clusters, k, stops increasing when doing so reduces total variability by less than this amount.  Default: 25%.
 """
 import sys
 from metaGene import *
@@ -15,71 +19,55 @@ from metaGene import *
 args = sys.argv  # [0]: script name
 
 clustering = 2
-shape_or_magnitude = -1
+shape_or_magnitude = 1
+ratio_of_files = 0
 sam_file = ""
+sam_file2 = ""
 gff_file = ""
 feature_type = ""
 ud_stream = 500
+norm_length = 1000
+dist_reduct	= 0.25
 
-if len(args) == 7:
-    try:
-        if args[1] == "-u":
-            clustering = 1
-        elif args[1] == "-c":
-            clustering = 2
-        elif args[1] == "-uc" or args[1] == "-cu":
-            clustering = 3
-        if args[2] == "-s":
-            shape_or_magnitude = 1
-        elif args[2] == "-m":
-            shape_or_magnitude = 0
-        sam_file = args[3]
-        gff_file = args[4]
-        feature_type = args[5]
-        ud_stream = int(args[6])
-
-    except:
-        print("Invalid input, closing program")
-        exit()
-elif len(args) == 6:
-    if args[1] == "-u":
-        clustering = 1
-    elif args[1] == "-c":
-        clustering = 2
-    elif args[1] == "-uc" or args[1] == "-cu":
-        clustering = 3
-    if args[2] == "-s":
-        shape_or_magnitude = 1
-        sam_file = args[3]
-        gff_file = args[4]
-        feature_type = args[5]
-    elif args[2] == "-m":
-        shape_or_magnitude = 0
-        sam_file = args[3]
-        gff_file = args[4]
-        feature_type = args[5]
-    else:
-        shape_or_magnitude = 1
-        sam_file = args[2]
-        gff_file = args[3]
-        feature_type = args[4]
-        ud_stream = int(args[5])
-elif len(args) == 5:
-    if args[1] == "-u":
-        clustering = 1
-    elif args[1] == "-c":
-        clustering = 2
-    elif args[1] == "-uc" or args[1] == "-cu":
-        clustering = 3
+argsNum = 1
+if args[argsNum] == "-u":
+    clustering = 1
+    argsNum = argsNum + 1
+elif args[argsNum] == "-c":
+    clustering = 2
+    argsNum = argsNum + 1
+elif args[argsNum] == "-uc" or args[argsNum] == "-cu":
+    clustering = 3
+    argsNum = argsNum + 1
+if args[argsNum] == "-s":
     shape_or_magnitude = 1
-    sam_file = args[2]
-    gff_file = args[3]
-    feature_type = args[4]
-
+    argsNum = argsNum + 1
+elif args[argsNum] == "-m":
+    shape_or_magnitude = 0
+    argsNum = argsNum + 1
+if args[argsNum] == "-r" or args[argsNum] == "-R":
+    ratio_of_files = 1
+    argsNum = argsNum + 1
+    sam_file = args[argsNum]
+    argsNum = argsNum + 1
+    sam_file2 = args[argsNum]
+    argsNum = argsNum + 1
+    gff_file = args[argsNum]
 else:
-    print("Incorrect number of arguements, closing program")
-    exit()
+    sam_file = args[argsNum]
+    argsNum = argsNum + 1
+    gff_file = args[argsNum]
+    argsNum = argsNum + 1
+feature_type = args[argsNum]
+argsNum = argsNum + 1
+ud_stream = int(args[argsNum])
+argsNum = argsNum + 1
+norm_length = int(args[argsNum])
+argsNum = argsNum + 1
+if len(args) >= argsNum:
+    dist_reduct = int(args[argsNum])
+    argsNum = argsNum + 1
 
-p = metaGenePlot(sam_file, gff_file, feature_type,
+p = metaGenePlot(sam_file, sam_file2, gff_file, feature_type,
                  ud_stream, clustering=clustering)
-p.plot("auto", ud_stream, d=shape_or_magnitude)
+p.plot("auto", norm_length, dist_reduct, d=shape_or_magnitude)
