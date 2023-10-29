f0ef87c40ab30d835d37ab65689583c50490349c
sdouglass1 <73650672+sdouglass1@users.noreply.github.com>
2023-10-29 17:11:50 -0400

    Update metaGene.py

--- metaGene.py
+++ metaGene.py
@@ -109,14 +109,14 @@ class metaGenePlot:
         """
         if sam_file2 == "":
             self.__samLines, self.__gffLines = self.__parseData(sam_file, gff_file)  # set file variables
-            self.computeRatio = false
+            self.computeRatio = False
         else:
             self.__samLines, self.__samLines2, self.__gffLines = self.__parseData2(sam_file, sam_file2, gff_file)  # set file variables
             self.__samLength2 = len(self.__samLines2)
             self.samLength2 = 0
             self.sam2 = sam_file2.split("/")[-1]
             self.__chrom2 = None
-            self.computeRatio = true
+            self.computeRatio = True
         self.__samLength = len(self.__samLines)
         self.__gffLength = len(self.__gffLines)
         self.sam = sam_file.split("/")[-1]
